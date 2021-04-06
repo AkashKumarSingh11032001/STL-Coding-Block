4f5a5abe07c38166fb00e1a1a2fb655931311ab5
Akash Kumar Singh <akashkumarsingh11032001@gmail.com>
2021-04-06 11:42:11 +0530

    Day6

---
+++ 1-Generic Programming/3_Itrator.cpp
@@ -0,0 +1,21 @@
+
+/*
+
+Iterators: Iterators as an entity that helps us to access the data within a container with certain restrictions.
+
+• Input Iterators:
+A entity through which you can read data from container and move ahead.
+For example - A Keyboard
+
+• Output Iterators
+A entity through which you can write into the container and move ahead.
+
+• Forward Iterators:
+Iterator with functionalities of input and output iterator in single direction. For Example - Singly Linked List
+
+• Bidirectional Iterators
+Forward Iterator that can move in both directions.
+
+• Random Access Iterators Random Access Iterator is an iterator that can read/write in both directions and also can take jumps.
+
+*/
