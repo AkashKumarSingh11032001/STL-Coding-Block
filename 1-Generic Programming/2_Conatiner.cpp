c48fca97ece6c5b61037b22850ac285f1f9b26ba
Akash Kumar Singh <akashkumarsingh11032001@gmail.com>
2021-04-06 11:33:10 +0530

    Day6

---
+++ 1-Generic Programming/2_Conatiner.cpp
@@ -0,0 +1,33 @@
+
+/*
+
+"Sequence Container"s: implement data structures which can be accessed in a sequential manner.
+
+o vector
+o list [double link list]
+o deque
+o arrays
+o forward list [singlre link list]
+
+"Container Adaptors" : provide a different interface for sequential containers.
+
+o queue
+o priority queue
+o stack
+
+"Associative Containers": implement sorted data structures that can be quickly searched (0(logn) complexity).
+
+o set
+o multiset
+o map
+o multimap
+
+"Unordered Associative Containers" : implement unordered data structures that can be quckly
+
+o unordered set
+o unordered multiset
+o unordered man
+o unordered multimap
+
+
+*/
