5f62849a1d8ae3378339bca85bd554a0d6a41c90
Akash Kumar Singh <akashkumarsingh11032001@gmail.com>
2021-04-07 14:47:49 +0530

    Day7

--- 2-Algorithm/3_SortFunction.cpp
+++ 2-Algorithm/3_SortFunction.cpp
@@ -1,12 +1,19 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() {
+// Define Comparator function
+bool compare(int a, int b)
+{
+    return a > b; // decreasing order and for incresing order reverse return (a < b)
+}
+
+int main()
+{
 
     int arr[] = {56, 10, 11, 11, 11, 57, 12, 3, 59, 74, 2, 51, 66};
     int n = sizeof(arr) / sizeof(int);
-    
-    cout << "Before" <<endl;
+
+    cout << "Before" << endl;
     for (int i = 0; i < n; i++)
     {
         /* code */
@@ -14,15 +21,13 @@ int main() {
     }
     cout << endl;
 
-    sort(arr,arr+n);
+    sort(arr, arr + n, compare);
 
-    cout << "After" <<endl;
+    cout << "After" << endl;
     for (int i = 0; i < n; i++)
     {
         /* code */
         cout << arr[i] << " ";
     }
     cout << endl;
-    
-    
 }
