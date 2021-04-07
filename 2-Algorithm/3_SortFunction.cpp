3a86e728abf0223ba51edbe98ce2898ebacb6c16
Akash Kumar Singh <akashkumarsingh11032001@gmail.com>
2021-04-07 13:32:27 +0530

    Day7

---
+++ 2-Algorithm/3_SortFunction.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+
+    int n,key;
+    cin >> n >> key;
+
+    int arr[100];
+    for (int i = 0; i < n; i++)
+    {
+        /* code */
+        cin >> arr[i];
+    }
+    
+
+
+    for (int i = 0; i < n; i++)
+    {
+        /* code */
+        cout << arr[i] << " ";
+    }
+    
+    
+}
