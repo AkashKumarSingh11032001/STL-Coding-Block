d6a7f79b7e16b0a2d50509edc92df7e541865026
Akash Kumar Singh <akashkumarsingh11032001@gmail.com>
2021-04-07 13:43:29 +0530

    Day7

--- 2-Algorithm/3_SortFunction.cpp
+++ 2-Algorithm/3_SortFunction.cpp
@@ -3,23 +3,26 @@ using namespace std;
 
 int main() {
 
-    int n,key;
-    cin >> n >> key;
-
-    int arr[100];
-    for (int i = 0; i < n; i++)
-    {
-        /* code */
-        cin >> arr[i];
-    }
+    int arr[] = {56, 10, 11, 11, 11, 57, 12, 3, 59, 74, 2, 51, 66};
+    int n = sizeof(arr) / sizeof(int);
     
-
-
+    cout << "Before" <<endl;
     for (int i = 0; i < n; i++)
     {
         /* code */
         cout << arr[i] << " ";
     }
+    cout << endl;
+
+    sort(arr,arr+n);
+
+    cout << "After" <<endl;
+    for (int i = 0; i < n; i++)
+    {
+        /* code */
+        cout << arr[i] << " ";
+    }
+    cout << endl;
     
     
 }
---
+++ 2-Algorithm/tempCodeRunnerFile.cpp
@@ -0,0 +1,2 @@
+int arr[] = {56, 10, 11, 11, 11, 57, 12, 3, 59, 74, 2, 51, 66};
+    int n = sizeof(arr) / sizeof(int);
