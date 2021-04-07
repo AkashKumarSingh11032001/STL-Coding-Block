112ae68c8af0c37738d83b3f031dbb8dacf0da1d
Akash Kumar Singh <akashkumarsingh11032001@gmail.com>
2021-04-07 16:37:23 +0530

    Day7

--- 2-Algorithm/4_Comparision.cpp
+++ 2-Algorithm/4_Comparision.cpp
@@ -4,18 +4,34 @@ using namespace std;
 
 bool compare(int a, int b)
 {
-    return a > b; // decreasing order and for incresing order reverse return (a < b)
+    return a < b; // decreasing order and for incresing order reverse return (a < b)
 }
 
 
-void bubbleSort(int arr[], int n) 
+void bubbleSort(int arr[], int n, bool (&cmp)(int a, int b)) 
 { 
     int i, j; 
     for (i = 0; i <= n-1; i++)     
       
     // Last i elements are already in place 
     for (j = 0; j <= n-i-1; j++) 
-        if (arr[j] > arr[j+1]) 
+        if (cmp(arr[j], arr[j+1])) 
             swap(arr[j], arr[j+1]); 
 }
 
+int main() {
+
+    int arr[] = {56, 10, 11, 57, 12, 3, 59, 74, 2, 51, 66};
+    int n = sizeof(arr) / sizeof(int);
+
+    bubbleSort(arr,n,compare);
+
+    for (int i = 0; i < n; i++)
+    {
+        /* code */
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+
+}
