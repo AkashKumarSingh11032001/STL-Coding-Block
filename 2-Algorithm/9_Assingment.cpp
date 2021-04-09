0dd8a3e6c4a76d6786d7871fb839b0cff3a6217d
Akash Kumar Singh <akashkumarsingh11032001@gmail.com>
2021-04-09 19:22:22 +0530

    Day9

---
+++ 2-Algorithm/9_Assingment.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+
+    int T;
+    cin >> T;
+
+    while (T--)
+    {
+        /* code */
+        int n;
+        cin >> n;
+        int arr[n];
+        for (int i = 0; i < n; i++)
+        {
+            /* code */
+            cin >> arr[i];
+        }
+        
+    }
+    
+}
