14b1245f47af66229d1ab51c6497ae46b4ebf6ea
Akash Kumar Singh <akashkumarsingh11032001@gmail.com>
2021-04-11 10:42:01 +0530

    Day11

--- 3-Strings/4_Design_String_Token.cpp
+++ 3-Strings/4_Design_String_Token.cpp
@@ -3,25 +3,45 @@ using namespace std;
 
 // Tokenization : breaking string into list of char
 
-int main(){
+char *mystrtok(char *s, char delm)
+{
+    //string s and delimeter delm
+
+    static char *input = NULL; // help to maintain the postion
+    if (s != NULL)
+    {
+        input = s;
+    }
+
+    // start extracting token and store in new array
+    char *output = new char[strlen(input) + 1];
+    int i = 0;
+    for (; input[i] != '\0'; i++)
+    {
+        if (input[i] != delm)
+        {
+            output[i] = input[i];
+        }
+        else
+        {
+            output[i] = '\0';
+            input += i + 1;
+            return output;
+        }
+    }
+}
+int main()
+{
 
     char str[100] = "Todaay is a rainy day";
 
-   
-    char *ptr = strtok(str," "); 
-   
-    cout << ptr <<endl; // out: Todaay
+    char *ptr = mystrtok(str, " ");
+    cout << ptr << endl;
 
-    // char *ptr = strtok(NULL," ");
-    // cout << ptr <<endl;  // out: is
-
-    while (ptr != NULL) // for geeting rest of the word
+    while (ptr != NULL)
     {
         /* code */
-        char *ptr = strtok(NULL," ");
-        cout << ptr <<endl;
-        
+        char *ptr = strtok(NULL, " ");
+        cout << ptr << endl;
     }
-    
-    
 }
