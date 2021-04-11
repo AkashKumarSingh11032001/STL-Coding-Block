5d160f4fc254b6067a08d06505abff404ab3e1ac
Akash Kumar Singh <akashkumarsingh11032001@gmail.com>
2021-04-11 09:28:29 +0530

    Day11

--- 3-Strings/2_String_Sorting.cpp
+++ 3-Strings/2_String_Sorting.cpp
@@ -1,37 +1,37 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-bool compare(string a, string b){
-    if(a.length() == b.length()){
+bool compare(string a, string b)
+{
+    if (a.length() == b.length())
+    {
         return a < b;
     }
     return a.length() > b.length(); //larger first
 }
 
-int main() {
+int main()
+{
 
     int n;
     cin >> n;
 
-    cin.get(); // conssume extra space
+    cin.get();       // conssume extra space
     string str[100]; // vector of str
     // inputing each char
     for (int i = 0; i < n; i++)
     {
         /* code */
-        getline(cin,str[i]);
+        getline(cin, str[i]);
     }
 
-
     // sorting
-    sort(str,str + n,compare);
+    sort(str, str + n, compare);
 
     //printing str
     for (int i = 0; i < n; i++)
     {
         /* code */
-        cout << str[i] <<endl;
+        cout << str[i] << endl;
     }
-
-
 }
