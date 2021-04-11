5fffb2775abfd0a19987741a51844e0bc2c3e657
Akash Kumar Singh <akashkumarsingh11032001@gmail.com>
2021-04-11 10:49:03 +0530

    Day11

--- 3-Strings/4_Design_String_Token.cpp
+++ 3-Strings/4_Design_String_Token.cpp
@@ -3,54 +3,55 @@ using namespace std;
 
 // Tokenization : breaking string into list of char
 
-char *mystrtok(char *s, char delm)
+char *mystrtok(char *str, char delim)
 {
-    //string s and delimeter delm
-
-    static char *input = NULL; // help to maintain the postion
-    if (s != NULL)
+    //string and single char acts as delimiter
+    static char *input = NULL;
+    if (str != NULL)
     {
-        input = s;
+        //we are maing the first call
+        input = str;
     }
-    // base case
-    if(input == NULL){
+    //check here -base case after the final token has been returned
+    if (input == NULL)
+    {
         return NULL;
     }
 
-    // start extracting token and store in new array
+    //start extracting tokens and store them in an array
     char *output = new char[strlen(input) + 1];
     int i = 0;
     for (; input[i] != '\0'; i++)
     {
-        if (input[i] != delm)
+        if (input[i] != delim)
         {
             output[i] = input[i];
         }
         else
         {
             output[i] = '\0';
-            input += i + 1;
+            input = input + i + 1;
             return output;
         }
     }
-
     //corner case
     output[i] = '\0';
     input = NULL;
     return output;
 }
+
 int main()
 {
 
-    char str[100] = "Todaay is a rainy day";
+    char s[100] = "Today, is a rainy, day";
 
-    char *ptr = mystrtok(str, " ");
+    char *ptr = mystrtok(s, ' ');
     cout << ptr << endl;
 
     while (ptr != NULL)
     {
-        /* code */
-        char *ptr = strtok(NULL, " ");
+        ptr = mystrtok(NULL, ' ');
         cout << ptr << endl;
     }
+    return 0;
 }
