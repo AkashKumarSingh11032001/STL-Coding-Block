4654f74b63dead2c06b60e4e3e78223e9e39ae37
Akash Kumar Singh <akashkumarsingh11032001@gmail.com>
2021-04-11 09:21:00 +0530

    Day11

--- 3-Strings/2_String_Sorting.cpp
+++ 3-Strings/2_String_Sorting.cpp
@@ -6,6 +6,19 @@ int main() {
 
     int n;
     cin >> n;
-    
-    string str[100];
+
+    cin.get(); // conssume extra space
+    string str[100]; // vector of str
+    // inputing each char
+    for (int i = 0; i < n; i++)
+    {
+        /* code */
+        getline(cin,str[i]);
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        /* code */
+        cout << str[i] <<endl;
+    }
 }
