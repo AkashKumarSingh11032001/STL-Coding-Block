652a0f1bbcaa6599216c749115fd809df2c18061
Akash Kumar Singh <akashkumarsingh11032001@gmail.com>
2021-04-13 15:08:08 +0530

    DAy13

--- 4-Vectors/4_countinuing _3 with_class.cpp
+++ 4-Vectors/4_countinuing _3 with_class.cpp
@@ -13,12 +13,35 @@ class Car{
         this->x = x;
         this->y = y;
     }
+    int distance(){
+        return x*x + y*y;
+    }
 };
 
-bool compare(){
+bool compare(Car A,Car B){
+    int da = A.distance();
+    int db = B.distance();
+    if(da == db){
+        return A.length() < B.length();
+    }
 
 }
 int main(){
 
     int n;
+    cin >> n;
+    vector<Car> v;
+    for (int i = 0; i < n; i++)
+    {
+        /* code */
+        int x,y;
+        string name;
+        cin >>name >> x >>y;
+        Car temp(name,x,y);
+        v.push_back(temp);
+    }
+    sort(v.begin(),v.end(),compare);
+    for(auto i:v){
+        cout << i.first <<","<<i.second<<endl;
+    }
 }
