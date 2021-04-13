918c8b1853ed14e15f2a20600bc2ae36b267d8e1
Akash Kumar Singh <akashkumarsingh11032001@gmail.com>
2021-04-13 14:43:41 +0530

    Day13

---
+++ 4-Vectors/4_countinuing _3 with_class.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Car{
+    public:
+    string name;
+    int x,y;
+    Car(){
+        
+    }
+}
+
+bool compare(){
+
+}
+int main(){
+
+    int n;
+}
