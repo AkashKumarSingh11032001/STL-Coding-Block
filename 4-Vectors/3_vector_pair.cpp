4420a0b4d19710f82d6ad028ccf3b7ecfe7ff7cd
Akash Kumar Singh <akashkumarsingh11032001@gmail.com>
2021-04-13 09:14:06 +0530

    Day13

---
+++ 4-Vectors/3_vector_pair.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+// V.IMP quetion regarding distance btw two point from orgin 
+//  2 way : using vector pait and by classes
+
+int main(){
+
+    int n;
+    cin >> n;
+    vector<pair<int,int>> v;
+    for (int i = 0; i < n; i++)
+    {
+        /* code */
+        int x,y;
+        cin >> x >>y;
+        v.push_back(make_pair(x,y));
+    }
+    
+
+
+}
