ecb55ed3e3d3ae2bd61aa58e0f988df9545e505b
Akash Kumar Singh <akashkumarsingh11032001@gmail.com>
2021-04-17 20:54:50 +0530

    Day17

--- 5-List n Forward List/forward_list.cpp
+++ 5-List n Forward List/forward_list.cpp
@@ -4,7 +4,15 @@ using namespace std;
 // implimenting a graph
 int main(){
 
-    
+    // list to create graph Data structur
+
+    list<pair<int,int>> *l; // dynamic list
+    int n;
+    cin >> n;
+
+    l = new list<pair<int,int>> [n];
+
+
 
 
 }
