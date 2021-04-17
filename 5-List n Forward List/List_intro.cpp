c24a58c09b5801df2e111e3dd5d0d20d967cc973
Akash Kumar Singh <akashkumarsingh11032001@gmail.com>
2021-04-17 20:11:51 +0530

    Day17

--- 5-List n Forward List/List_intro.cpp
+++ 5-List n Forward List/List_intro.cpp
@@ -9,9 +9,9 @@ int main(){
     //inilizeation
     list<int> l2{1,2,3,4};
     //diffrent datatype
-    list<string> l3{'a','b','c'};
+    list<string> l3{"adc","bdcs","cdcs"};
     // add element 
-    l3.push_back('d');
+    l3.push_back("ddc");
 
     // itrate over list
     for(auto x:l3){
