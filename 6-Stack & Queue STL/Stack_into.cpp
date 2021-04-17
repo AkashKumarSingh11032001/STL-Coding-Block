6b4da80db1d623d6dd608c7918d68c69972f3205
Akash Kumar Singh <akashkumarsingh11032001@gmail.com>
2021-04-17 21:40:09 +0530

    DAy17

--- 6-Stack & Queue STL/Stack_into.cpp
+++ 6-Stack & Queue STL/Stack_into.cpp
@@ -3,7 +3,25 @@ using namespace std;
 
 
 //  STACK -> LIFO
+// method present: -> push(),pop(),top(),empty(),size()
+
 int main(){
+
+    stack<int> s;
+
+    for (int i = 0; i <= 5; i++)
+    {
+        /* code */
+        s.push(i);
+    }
+    
+    //Loop
+    while (!s.empty())
+    {
+        /* code */
+        cout << s.top() <<" ";
+        s.pop();
+    }
     
 
 }
