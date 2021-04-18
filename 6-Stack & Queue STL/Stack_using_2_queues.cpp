127e9db20e6afe419a4c9f064ba670fee6ecfb36
Akash Kumar Singh <akashkumarsingh11032001@gmail.com>
2021-04-18 21:22:33 +0530

    Day18

--- 6-Stack & Queue STL/Stack_using_2_queues.cpp
+++ 6-Stack & Queue STL/Stack_using_2_queues.cpp
@@ -1,6 +1,38 @@
 #include <bits/stdc++.h>
 using namespace std;
 // STACK : using 2 queues
+template<typename T>
+class Stack{
+    queue<T> q1,q2;
+public:
+    void push(T x){
+         q1.push(x);
+    }
+
+    void pop(){
+        // remove last ellemt from q1
+        // we have to move first n-1 elem in q2
+        //interchange the name of q1 and q2
+        if(q1.empty()){
+            return;
+        }
+
+        while (q1.size() > 1)
+        {
+            /* code */
+            T element = q1.front();
+            q2.push(element);
+            q1.pop();
+        }
+        // remove last ele 
+        q1.pop();
+
+        //swap the name of q1 and q2
+        swap(q1,q2);
+        
+    }
+
+};
 int main(){
 
 }
